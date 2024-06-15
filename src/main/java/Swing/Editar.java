f01d84876037e2cd7d61ad927843ad5b0c8118e1
Gabriel Cândido Ribeiro da Silva <>
2024-06-15 19:54:31 -0300

    Polimento do codigo (Licença e Author nas classes)

--- src/main/java/Swing/Editar.java
+++ src/main/java/Swing/Editar.java
@@ -1,6 +1,21 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ * Copyright (C) 2024 Gabriel Cândido Ribeiro da Silva <gcrs2018@gmail.com> 
+ * Copyright (C) 2024 Gustavo Rezende Gabriel <gustavorezendegabriel@hotmail.com>
+ * Copyright (C) 2024 João Vitor Bier Barros <joaovitorbierbarros@gmail.com>
+ * Copyright (C) 2024 Theo da Silva Sa <03silva.theo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package Swing;
 
@@ -19,7 +34,10 @@ import javax.swing.text.AbstractDocument;
 
 /**
  *
- * @author Aluno
+ * @author Gabriel Cândido Ribeiro da Silva <gcrs2018@gmail.com>
+ * @author Gustavo Rezende Gabriel <gustavorezendegabriel@hotmail.com>
+ * @author João Vitor Bier Barros <joaovitorbierbarros@gmail.com>
+ * @author Theo da Silva Sa <03silva.theo@gmail.com>
  */
 public class Editar extends javax.swing.JFrame {
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2024 Gabriel Cândido Ribeiro da Silva <gcrs2018@gmail.com> 
+ * Copyright (C) 2024 Gustavo Rezende Gabriel <gustavorezendegabriel@hotmail.com>
+ * Copyright (C) 2024 João Vitor Bier Barros <joaovitorbierbarros@gmail.com>
+ * Copyright (C) 2024 Theo da Silva Sa <03silva.theo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.example;
 import Model.City;
 import Services.*;
@@ -9,6 +28,16 @@ import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriel Cândido Ribeiro da Silva <gcrs2018@gmail.com>
+ * @author Gustavo Rezende Gabriel <gustavorezendegabriel@hotmail.com>
+ * @author João Vitor Bier Barros <joaovitorbierbarros@gmail.com>
+ * @author Theo da Silva Sa <03silva.theo@gmail.com>
+ * @date 03/06/2024
+ * @brief Class Main
+ */
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
